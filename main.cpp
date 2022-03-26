dda16bcbf248fe15c91cc5c9f4b8f63af9f7fab7
Emelda Torren <etorren@si-p3.kzn.21-school.ru>
2022-03-26 14:22:32 +0300

    vector begin/ without iterators

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <deque>
+#if 0 //CREATE A REAL STL EXAMPLE
+	#include <map>
+	#include <stack>
+	#include <vector>
+	namespace ft = std;
+    #define STD     1
+#else
+	// #include "map.hpp"
+	// #include "stack.hpp"
+	#include "vector.hpp"
+    #define STD     0
+#endif
+
+#include <stdlib.h>
+
+#define MAX_RAM 4294967296
+#define BUFFER_SIZE 4096
+struct Buffer
+{
+	int idx;
+	char buff[BUFFER_SIZE];
+};
+
+
+void    cont_data(const int *p, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << "i = " << i << " >> " << +p[i] << "\n";
+    }
+}
+
+#define COUNT (MAX_RAM / (int)sizeof(Buffer))
+int     main(void)
+{
+    STD ? std::cout << "\e[34mstd\e[31m::container\e[0m\n" : std::cout << "\e[32mft\e[31m::container\e[0m\n";
+    ft::vector<int> arr;
+    arr.push_back(63);
+    arr.push_back(65);
+    arr.push_back(66);
+    arr.push_back(66);
+    arr.push_back(66);
+    // arr.reserve(10);
+    // std::cout << arr.back() << "\n";
+    // std::cout << ">>= " << arr.back() << "\n";
+    // arr.pop_back();
+    arr.reserve(15);
+    arr.resize(10);
+    // cont_data(arr.data(), arr.size());
+    // arr.clear();
+    // cont_data(arr.data(), arr.size());
+    ft::vector<int> opt;
+    opt = arr;
+    opt.assign(3, 4);
+    // // arr.swap(opt);
+    cont_data(opt.data(), opt.size());
+    // std::cout << ">>= " << arr[0] << "\n";
+    // arr.push_back(3);
+    // arr.push_back(3);
+    // arr.push_back(3);
+    // arr.reserve(4);
+    std::cout << "\e[33msize = " << opt.size() << "\n";
+    std::cout << "\e[33mcapacity = " << opt.capacity() << "\n";
+    std::cout << "\e[33mempty = " << opt.empty() << "\n";
+    std::cout << "\e[33mmax_size = " << opt.max_size() << "\n";
+    return (0);
+}
---
+++ vector.hpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <stdexcept>
+
+namespace ft {
+template<class T, class Allocator = std::allocator<T> >
+class vector
+{
+	public:
+		typedef	T					value_type;
+		typedef std::allocator<T> 	allocator_type;
+		typedef size_t		 		size_type;
+		typedef	std::ptrdiff_t		difference_type;
+		typedef value_type&			reference;
+		typedef const value_type&	const_reference;
+		typedef typename Allocator::pointer	pointer;
+		typedef typename Allocator::const_pointer const_pointer;
+		//typedef 	iterator;
+		//typedef	const_iterator;
+		//typedef	reverse_iterator;
+		//typedef	const_reverse_iterator;
+
+
+		vector( void ) 
+		{ 
+			_container = NULL;
+			_size = 0; 
+			_capacity = 0;
+		}
+
+		~vector( void )
+		{ 
+			for (size_type i = 0; i < _size + 1; i++)
+				_allocator.destroy(_container + i);
+			_allocator.deallocate(_container, _capacity + 1);
+		}
+
+
+		size_type	max_size( void ) const
+		{
+			return std::numeric_limits<difference_type>::max() / (sizeof(T) > 2 ? sizeof(T) / 2 : sizeof(T));
+		}
+
+		void reserve( size_type new_cap )
+		{
+			if (new_cap > max_size()) throw std::length_error("vector");
+			if (new_cap > _capacity)
+			{
+				T *tmp = _allocator.allocate(new_cap + 1);
+				for (size_type i = 0; i < _size; i++)
+				{
+					_allocator.construct(tmp + i, _container[i]);
+					_allocator.destroy(_container + i);
+				}
+				_allocator.deallocate(_container, _capacity + 1);
+				_capacity = new_cap;
+				_container = tmp;
+			}
+		}
+
+		void	push_back( const T& value )
+		{
+			if (!_capacity)
+				reserve(1);
+			else if (_size >= _capacity)
+				reserve(2 * _capacity);
+			_allocator.construct(_container + _size++, value );
+		}
+
+		void	pop_back( void )
+		{
+			_allocator.destroy(_container + _size--);
+		}
+
+		void resize( size_type count, T value = T() )
+		{
+			if (count > _size)
+			{
+				reserve(count);
+				for ( ;_size < count ;_size++ )
+					_allocator.construct(_container + _size, value);
+			}
+			else if (count < _size)
+			{
+				for ( ; count < _size; _size--)
+					_allocator.destroy(_container + _size);
+			}
+		}
+
+		void clear( void )
+		{
+			for ( ; _size > 0; _size--)
+				_allocator.destroy(_container + _size);
+			_allocator.destroy(_container);	
+		}
+
+		vector& operator=( const vector& other )
+		{
+			if (this == &other)
+				return *this;
+			_capacity = 0;
+			reserve(other.size());
+			for(size_type i = 0; i < other.size(); i++)
+				push_back(other[i]);
+			return *this;
+		}
+
+		void assign( size_type count, const T& value )
+		{
+			clear();
+			if (count > _capacity)
+				reserve(count);
+			for ( ;_size < count; _size++)
+				_allocator.construct(_container + _size, value);
+		}
+
+		// template< class InputIt >
+		// void assign( InputIt first, InputIt last )
+		// {
+
+		// }
+
+		allocator_type get_allocator() const { return _allocator; }
+
+		reference operator[]( size_type pos ) { return (_container[pos]); }
+		const_reference operator[]( size_type pos ) const { return(_container[pos]); }
+
+		reference at( size_type pos ) { if (_size <= pos) throw std::out_of_range("vector"); return(_container[pos]); }
+		const_reference at( size_type pos ) const { if (_size <= pos) throw std::out_of_range("vector"); return(_container[pos]); }
+
+		reference back( void ) { return(_container[_size - 1]); }
+		const_reference back( void ) const { return(_container[_size - 1]); }
+
+		reference & front( void ) { return(_container[0]); }
+		const_reference & front( void ) const { return(_container[0]); }
+
+		size_type size( void ) const { return(_size); }
+
+		size_type capacity( void ) const { return(_capacity); }
+
+		bool	empty( void ) const { return !(_size); }
+
+		T * data( void ) { return _container; }
+		const T * data( void ) const { return _container; }
+
+		private:
+			T *				_container;
+			size_type		_size;
+			size_type		_capacity;
+			allocator_type	_allocator;
+};
+}
